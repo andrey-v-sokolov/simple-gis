2604066c83d7c5016fecd33454d9cc5f3d0c0222
Andrey Sokolov <andrey.v.sokolov@gmail.com>
2017-07-21 19:37:02 +0700

    Dao WIP;

--- service/src/main/java/com/simplegis/webservice/persistence/dao/CityDao.java
+++ service/src/main/java/com/simplegis/webservice/persistence/dao/CityDao.java
@@ -1,12 +1,79 @@
 package com.simplegis.webservice.persistence.dao;
 
-
+import com.simplegis.webservice.persistence.entity.City;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
- * Dao.
+ * Cities dao.
  */
 @Repository
 public class CityDao {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    /**
+     * Get all cities.
+     *
+     * @return list of cities.
+     */
+    public List<City> getAllCities() {
+        return jdbcTemplate.query("SELECT * FROM simplegisdb.city", new BeanPropertyRowMapper<>(City.class));
+    }
+
+    /**
+     * Get city by id.
+     *
+     * @param id of a city
+     * @return city or null
+     */
+    public City getCityById(Long id) {
+        return jdbcTemplate.queryForObject("SELECT * FROM simplegisdb.city WHERE id = ?", new Object[]{id}, new BeanPropertyRowMapper<>(City.class));
+    }
+
+    /**
+     * Update city by id.
+     *
+     * @param city with new data
+     * @return 1 if updated 0 if not
+     */
+    public Integer updateCityById(City city) {
+        return jdbcTemplate.update("UPDATE simplegisdb.city c SET c.name = ?, c.area = ?, c.population = ? WHERE c.id = ?",
+                city.getName(), city.getArea(), city.getPopulation(), city.getId());
+    }
+
+    /**
+     * Add city.
+     *
+     * @param city to insert into db
+     * @return inserted city
+     */
+    public City insertCity(City city) {
+
+        /*
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update("INSERT INTO simplegisdb.city (name, area, population) VALUES (?, ?, ?)",
+                new Object[] {city.getName(), city.getArea(), city.getPopulation()},
+                keyHolder, new String[]{"id"});
+        city.setId(keyHolder.getKey().longValue());
+        */
+        return city;
+    }
+
+    /**
+     * Batch cities add.
+     *
+     * @param cities list fo cities to insert into db
+     * @return list of inserted cities
+     */
+    public List<City> batchInsertCities(List<City> cities) {
+        return null;
+    }
 }
--- service/src/main/java/com/simplegis/webservice/web/TestController.java
+++ service/src/main/java/com/simplegis/webservice/web/TestController.java
@@ -1,9 +1,14 @@
 package com.simplegis.webservice.web;
 
+import com.simplegis.webservice.persistence.dao.CityDao;
+import com.simplegis.webservice.persistence.entity.City;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
 
 /**
  * Test controller.
@@ -13,12 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
 
+    @Autowired
+    private CityDao cityDao;
+
     /**
      * Test endpoint.
      * @return string.
      */
     @RequestMapping("/test")
     public String test() {
+
+        cityDao.getAllCities().forEach(city -> LOG.debug(city.toString()));
+        City city = new City();
+        city.setName("test");
+        city.setArea(BigDecimal.ONE);
+        city.setPopulation(1);
+        cityDao.insertCity(city);
 
         LOG.debug("it works");
         return "it works";
